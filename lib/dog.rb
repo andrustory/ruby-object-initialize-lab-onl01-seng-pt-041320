563794f3ffe60d965569ec8b2421d2dde358a2e7
Andres Carvajal <jc1993box@hotmail.com>
2020-04-30 04:35:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,16 @@
 class Dog 
-  attr_accessor :name, :breed
   
 def initialize(dog_name, dog_breed)
   @name dog_name
   @breed = dog_breed
+  
+def name= (dog_name)
+  @name = dog_name
 end
 
-
+def name
+  @name
+end
 end
 
 
